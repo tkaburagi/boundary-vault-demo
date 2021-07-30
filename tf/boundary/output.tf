851f80306a39d4b4b6cf56c93a0e53d783a2ed95
tkaburagi <t.kaburagi@me.com>
2021-07-30 17:27:20 +0900

    enhanced output

--- tf/boundary/output.tf
+++ tf/boundary/output.tf
@@ -1,25 +1,23 @@
 //target id
-output "ssh-gcp-target-id" {
-  value = boundary_target.ssh-gcp-target.id
-}
-
-output "ssh-aws-target-id" {
-  value = boundary_target.ssh-aws-target.id
-}
-
-output "psql-target-id" {
-  value = boundary_target.psql-target.id
-}
-
-output "mysql-target-id" {
-  value = boundary_target.mysql-target.id
-}
-
-output "rdp-target-id" {
-  value = boundary_target.rdp-target.id
+output "target-ids" {
+  value = [
+    "GCP VM: ${boundary_target.ssh-gcp-target.id}",
+    "AWS VM: ${boundary_target.ssh-aws-target.id}",
+    "AZURE RDP: ${boundary_target.rdp-target.id}",
+    "LOCAL MYSQL: ${boundary_target.mysql-target.id}",
+    "LOCAL POSTGRES: ${boundary_target.psql-target.id}",
+    "GKE K8S: ${boundary_target.gke-target.id}",
+  ]
 }
 
 //password
 output "dbadmin-password" {
-  value = [boundary_account.dbadmin.login_name,boundary_account.dbadmin.password]
+  value = [boundary_account.dbadmin.login_name, boundary_account.dbadmin.password]
+}
+
+output "auth-method" {
+  value = [
+    "PASSWORD: ${boundary_auth_method.password.id}",
+    "AZURE AD: ${boundary_auth_method_oidc.oidc_azuread.id}"
+    ]
 }
